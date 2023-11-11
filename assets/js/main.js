f5bdefaffb277526d55666dfdfa9b72094281884
CGHSPCI <143050803+CGHSPCI@users.noreply.github.com>
2023-11-11 13:34:38 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,3 +254,42 @@ function showOptions() {
 document.getElementById('tableSelector').addEventListener('change', function() {
   this.size = 1; // Reset size to 1 after an option is selected
 });
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  fade: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    430: {
+      slidesPerView: 2,
+    },
+   860: {
+      slidesPerView: 3,
+    },
+    1290: {
+      slidesPerView: 4,
+    },
+  },
+  autoplay: {
+    delay: 2000,
+  },
+});
+
